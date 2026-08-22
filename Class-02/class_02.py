8bda8fd471278077a09381d5739d0af4737c18da
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-08-22 19:38:13 +0500

    Class-02

---
+++ Class-02/class_02.py
@@ -0,0 +1,12 @@
+# concat the number, apas me doo number ko join krna
+salary = 50000;
+bonus = 10000;
+totalSalary = salary + bonus;
+print(salary)
+print(bonus)
+print(totalSalary)
+
+# user se data lena hai tw kiya input lagana prega
+# userName = input("Enter your name: ");
+# print(userName)
+# print("Welcome:", userName)
