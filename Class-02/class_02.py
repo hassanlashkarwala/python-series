4b6081466fb2696eadf35cbece30cdd42d4509ad
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-08-29 01:38:58 +0500

    class-02

--- Class-02/class_02.py
+++ Class-02/class_02.py
@@ -1,12 +1,58 @@
 # concat the number, apas me doo number ko join krna
-salary = 50000;
-bonus = 10000;
-totalSalary = salary + bonus;
-print(salary)
-print(bonus)
-print(totalSalary)
+# salary = 50000;
+# bonus = 10000;
+# totalSalary = salary + bonus;
+# print(salary)
+# print(bonus)
+# print(totalSalary)
 
 # user se data lena hai tw kiya input lagana prega
 # userName = input("Enter your name: ");
 # print(userName)
 # print("Welcome:", userName)
+
+# age = input("Enter your age: ");
+# print("Your current age is: ", age);
+# print(type(age));
+
+# type casting aik data type ko dosri data type me convert krna
+# input hamesha string type return kre gi agr mujhe integer ya number me convert krna hai tw i used int()
+
+# age = int(input("Enter your age: "));
+# print("Your age after 10 years will be: ", age + 10);   
+
+# integer me data type convert krni hai tw int()
+# float me data type convert krni hai tw float()
+# string me data type convert krni hai tw str()
+# boolean me data type convert krni hai tw bool()
+
+# name, age, city (ask user for input and then print each)
+# name = input("Enter your name: ");
+# age = int(input("Enter your age: "));
+# city = input("Enter your city: ");
+# print("Name: ", name);
+# print("Age: ", age);
+# print("City: ", city);
+
+# shopping calculator
+# product = input("Which thing do you purchase?");
+# price = int(input("your product price "));
+# quantity = int(input("how much quantity "));
+# total = price * quantity;
+# print("Product name is: ", product)
+# print("Product price is: ", price)
+# print("Product quantity is: ", quantity)
+# print("Total cost of your product is: ", total)
+
+# String Concatenation
+# firstName = "Hassan";
+# lastName = "Raza";
+# fullName = firstName + " " + lastName;
+# print(fullName);
+
+# two data types ko aps me concat kr sakte hen
+# a = "Hassan "
+# print(a * 10)
+
+# print(2 + "2")  ye error dega bcz python confused ho jati h ke aik string and aik inetger apas me concat nh ho gay
+# print("2" + "2")  ye error nh dega bcz boths are string and apas me concat ho jai gay
